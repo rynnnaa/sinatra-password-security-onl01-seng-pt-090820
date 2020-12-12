ecf34b86a359d28dc63f80d17f1d9ba1cf85911b
Ryna Truong <rynatruong@outlook.com>
2020-12-12 21:49:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,9 +19,9 @@ class ApplicationController < Sinatra::Base
 	post "/signup" do
 		user = User.new(:username => params[:username], :password => params[:password])
 		if user.save
-		  redirect "/login"
+			redirect "/login"
 		else
-		  redirect "/failure"
+			redirect "/failure"
 		end
 	end
 
